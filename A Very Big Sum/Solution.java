a30c5b9ee4358514b075ba39fc4b1f776202e9b0
Arsène Mujyabwami <181954@umons.ac.be>
2026-07-26 21:42:55 +0200

    First Problem Solving badge

---
+++ A Very Big Sum/Solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar, int arCount) {
+        // Write your code here
+        long sum = 0;
+        for(int i = 0; i<arCount; i++){
+            sum += ar.get(i);
+        }
+        return sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String outputPath = System.getenv("OUTPUT_PATH");
+        BufferedWriter bufferedWriter = outputPath == null
+                ? new BufferedWriter(new OutputStreamWriter(System.out))
+                : new BufferedWriter(new FileWriter(outputPath));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar, arCount);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Simple Array Sum/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<Integer> ar, int arCount) {
+        // Write your code here
+        int sum = 0;
+        for(int i = 0; i<arCount; i++){
+            sum += ar.get(i);
+        }
+        return sum;
+    }
+
+}
+
+//on powershell: Get-Content .\Sample.txt | java Solution
+//on cmd: java Solution < Sample.txt
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String outputPath = System.getenv("OUTPUT_PATH");
+        BufferedWriter bufferedWriter = outputPath == null
+                ? new BufferedWriter(new OutputStreamWriter(System.out))
+                : new BufferedWriter(new FileWriter(outputPath));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.simpleArraySum(ar, arCount);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
